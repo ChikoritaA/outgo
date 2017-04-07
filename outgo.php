98c19218a29bd5a2a8bee7e3dee679d7afc6fbe2
ChikoritaA <dontworry53113@gmail.com>
2017-04-06 20:56:00 -0700

    Create outgo.php

---
+++ outgo.php
@@ -0,0 +1,29 @@
+<?php
+$link1 = $_GET['4hfrDE9uMxbK6ahvd4DDbemyRFLM6nx36YeDgB9xCzMQ39erCP963w733gvetjPNMFWVXJxFAgQNzpUAqwS8zgf76rQpDRc3cwmsQtPXStBARCAUJ3dWKPKr9EjpPypw47nJbKNy6q7N5KzEu854pzGb3g3D92CNJ3pBznBryjrYBNqLM3BzRUt9DSXhSWATu6txnw3CsKhkwZfjXFTst9rYmKpcem93gsg8EAh97kUXHCEDZg2QeTqEQYZtrBNu'];
+$link2 = $_GET['HAdrqQShypLzrJh9YdDfcM4kz2J5jcajhR8tGMWnu3fX9uJpU8TMfx5nN4ZfdryTvgw2J9ma6SNcL3qqmKgkasT3pVN4DuzwGQC4eNCDz4ERhKUrAmqfk9Amc9AL'];
+$link3 = $_GET['vnQRY7nKS69YBZup7fMMZX7sKqthbTweX5Nq92qLydQS4zpNNwWwKfMCkHpc894uK8NjBCFEpc2Ym5wD9vLxYjPgX2Qu9hDtt3vVBgW8RNQJuJHNLV2a2yr3HfpLC6EF'];
+?>
+<html>
+	<head>
+	<meta name="ROBOTS" content="NOINDEX, NOFOLLOW">
+		<title>AnimeW- Estas saliendo de nuestros dominios</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+		<meta http-equiv="REFRESH" content="5; url=https://shink.in/<?php echo $link1; ?>">
+		<script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*newDate();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');ga('create', 'UA-89651660-1', 'auto');ga('send', 'pageview');</script>
+<script>
+function disabletext(e){return false}function reEnable(){return true}document.onselectstart=new Function ("return false")if (window.sidebar){document.onmousedown=disabletextdocument.onclick=reEnable}</script>  
+<style>.animewnet{background:-webkit-gradient(linear,left top,left bottom,color-stop(.05,#768d87),color-stop(1,#6c7c7c));background:-moz-linear-gradient(top,#768d87 5%,#6c7c7c 100%);background:-webkit-linear-gradient(top,#768d87 5%,#6c7c7c 100%);background:-o-linear-gradient(top,#768d87 5%,#6c7c7c 100%);background:-ms-linear-gradient(top,#768d87 5%,#6c7c7c 100%);background:linear-gradient(to bottom,#768d87 5%,#6c7c7c 100%);filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#768d87', endColorstr='#6c7c7c', GradientType=0);background-color:#768d87;-moz-border-radius:5px;-webkit-border-radius:5px;border-radius:5px;border:1px solid #566963;display:inline-block;cursor:pointer;color:#fff;font-family:Georgia;font-size:20px;padding:11px 23px;text-decoration:none}.animewnet:hover{background:-webkit-gradient(linear,left top,left bottom,color-stop(.05,#6c7c7c),color-stop(1,#768d87));background:-moz-linear-gradient(top,#6c7c7c 5%,#768d87 100%);background:-webkit-linear-gradient(top,#6c7c7c 5%,#768d87 100%);background:-o-linear-gradient(top,#6c7c7c 5%,#768d87 100%);background:-ms-linear-gradient(top,#6c7c7c 5%,#768d87 100%);background:linear-gradient(to bottom,#6c7c7c 5%,#768d87 100%);filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#6c7c7c', endColorstr='#768d87', GradientType=0);background-color:#6c7c7c}.animewnet:active{position:relative;top:1px}</style>
+	</head>
+	<body>
+		<div align="center">
+			<h1>Seras redireccionado ala URL especificada en 5 segundos..</h1>
+			<h3>Regresa pronto, y disfruta tu descarga</h3>
+			<h3>Attentamente AnimeW.net</h3>
+			<img src="img/redirect.gif" align="absmiddle" />
+			<p>En caso de error oprime el boton</p>
+			<a href="https://shink.in/<?php echo $link1; ?>" class="animewnet">Opcion 1</a>
+			<a href="http://short.am/<?php echo $link2; ?>" class="animewnet">Opcion 2</a>
+			<div align="center"><a href="http://viid.me/<?php echo $link3; ?>" rel="nofollow">Opcion 3(Conexiones Lentas)</a></div>
+		</div>
+	</body>
+</html>
